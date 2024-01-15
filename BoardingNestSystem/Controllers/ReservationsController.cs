9b1e818b219fe95ba3b4a9ef4342ff53bd35dbe7
jackmoroscallo <jackmoroscallo@dmc.edu.ph>
2024-01-15 20:02:33 +0800

    changes 1/15/24

--- BoardingNestSystem/Controllers/ReservationsController.cs
+++ BoardingNestSystem/Controllers/ReservationsController.cs
@@ -1,6 +1,7 @@
 ﻿using BoardingNestSystem.Data;
 using BoardingNestSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BoardingNestSystem.Controllers
@@ -113,6 +114,58 @@ namespace BoardingNestSystem.Controllers
         private bool ReservationsExists(Guid id)
         {
             return (_context.Reservations?.Any(e => e.BoardingHouseID == id)).GetValueOrDefault();
+        }
+
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Reservations == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            ViewData["BoardingHouseID"] = new SelectList(_context.BoardingHouses, "BoardingID", "BoardingID", reservation.BoardingHouseID);
+            return View(reservation);
+        }
+
+        // POST: RentInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("ReservationId,BoardingHouseID,ClientsName,ClientsNumber,DateCheckIn,DateCheckOut,IsFinished")] Reservation reservation)
+        {
+            if (id != reservation.ReservationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationsExists(reservation.ReservationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(IndexReservation));
+            }
+            ViewData["BoardingID"] = new SelectList(_context.BoardingHouses, "BoardingID", "BoardingID", reservation.BoardingHouseID);
+            return View(reservation);
         }
     }
 }
